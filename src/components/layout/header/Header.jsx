61dcba2f745bed02539286b060a4dea862b992bf
Kamil Grabek <kamil.grabek@allegro.pl>
2019-05-25 15:24:44 +0200

    poprawny order importów w Header.jsx

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,8 +1,8 @@
+import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import { withStyles } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import React from 'react';
 
 const styles = theme => ({
   appBar: {

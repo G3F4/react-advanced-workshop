a1fe54837fdb2fd277ad47a52520ab792780f3e4
Kamil Grabek <kamil.grabek@allegro.pl>
2019-05-25 16:59:30 +0200

    Updates

---
+++ precache-manifest.91aec01e0aca2b6d56de42137f642d0d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c46894212017783c1ecd51de996f0e21",
+    "url": "/warsawjs-workshop-32-book-it/index.html"
+  },
+  {
+    "revision": "4bd7e314dae926c3a80d",
+    "url": "/warsawjs-workshop-32-book-it/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "17574922d5d132654bed",
+    "url": "/warsawjs-workshop-32-book-it/static/js/0.0c391965.chunk.js"
+  },
+  {
+    "revision": "8dd3ce0e406cd62638ff",
+    "url": "/warsawjs-workshop-32-book-it/static/js/3.3b99e9f7.chunk.js"
+  },
+  {
+    "revision": "8a37e6b4c310ff1b8f8a",
+    "url": "/warsawjs-workshop-32-book-it/static/js/4.5c66409b.chunk.js"
+  },
+  {
+    "revision": "9cc06269c75864cfd87a",
+    "url": "/warsawjs-workshop-32-book-it/static/js/5.15048161.chunk.js"
+  },
+  {
+    "revision": "871c679a11c1834b8504",
+    "url": "/warsawjs-workshop-32-book-it/static/js/6.550c9ead.chunk.js"
+  },
+  {
+    "revision": "e055900b23436037fc2c",
+    "url": "/warsawjs-workshop-32-book-it/static/js/7.0da27d49.chunk.js"
+  },
+  {
+    "revision": "4bd7e314dae926c3a80d",
+    "url": "/warsawjs-workshop-32-book-it/static/js/main.8fb21444.chunk.js"
+  },
+  {
+    "revision": "7478d31fe356490d1499",
+    "url": "/warsawjs-workshop-32-book-it/static/js/runtime~main.976540c4.js"
+  }
+]);

cb68d8a4f10dbf7c73013b578991318de932a349
Kamil Grabek <kamil.grabek@allegro.pl>
2019-05-25 15:22:39 +0200

    usunięcie App.css i Header jako functional

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css';
 import ErrorBoundary from './components/layout/error-boundary/ErrorBoundary';
 import Header from './components/layout/header/Header';
 
--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
+import { withStyles } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import { withStyles } from '@material-ui/core/styles';
+import React from 'react';
 
 const styles = theme => ({
   appBar: {
@@ -10,20 +10,14 @@ const styles = theme => ({
   },
 });
 
-class Header extends React.Component {
-  render() {
-    const { classes } = this.props;
-
-    return (
-      <AppBar position="static" color="default" className={classes.appBar}>
-        <Toolbar>
-          <Typography variant="h6" color="inherit">
-            <a href="/">BOOK IT</a>! THE BEST BOOKING HELPER!
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    );
-  }
-}
+const Header = ({ classes }) => (
+  <AppBar position="static" color="default" className={classes.appBar}>
+    <Toolbar>
+      <Typography variant="h6" color="inherit">
+        <a href="/">BOOK IT</a>! THE BEST BOOKING HELPER!
+      </Typography>
+    </Toolbar>
+  </AppBar>
+);
 
 export default withStyles(styles)(Header);

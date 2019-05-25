79f0fe38c83df5bac7d29ccfeb9ba316a56e4f34
Kamil Grabek <kamil.grabek@allegro.pl>
2019-05-25 16:46:10 +0200

    import fix

--- src/components/views/accommodation-list/list/List.jsx
+++ src/components/views/accommodation-list/list/List.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
@@ -11,8 +13,6 @@ import { withStyles } from '@material-ui/core/styles';
 import Tab from '@material-ui/core/Tab';
 import Tabs from '@material-ui/core/Tabs';
 import Typography from '@material-ui/core/Typography';
-import PropTypes from 'prop-types';
-import React from 'react';
 import Price from '../../../common/price/Price';
 import Rating from '../../../common/rating/Rating';
 import Share from '../../../common/share/Share';
